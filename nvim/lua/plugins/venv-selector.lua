35cfe7899a91c83efb4d6b52fed3e8fee600f921
John Blomberg <john.jb.blomberg@gmail.com>
2024-06-26 23:09:19 +0200

    nvim: Remove deprecated venv-selector command

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -14,6 +14,6 @@ return {
     -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>VenvSelect<cr>' },
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    -- { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
   },
 }
