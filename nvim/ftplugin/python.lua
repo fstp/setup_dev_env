fe8b05f4556895c08f1f1646b5b784ade6a405c1
John Blomberg <john.jb.blomberg@gmail.com>
2024-07-18 09:58:46 +0200

    Add some Python utilities for running code

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -2,26 +2,19 @@ local i = function(x)
   print(vim.inspect(x))
 end
 
-local function send_to_tmux(lines, pane)
-  -- Create a temporary file to hold the lines
+local function send_to_tmux(text, pane)
   local tmpname = os.tmpname()
   local f = io.open(tmpname, "w+")
   if f == nil then
-    print("Could not open temporary file")
-    return
+    error("Could not open temporary file")
   end
 
-  for _, line in ipairs(lines) do
-    f:write(line .. "\n")
-  end
-
+  f:write(text .. "\n")
   f:close()
 
-  -- Send the contents of the temporary file to the tmux pane
-  local cmd = "tmux load-buffer " .. tmpname .. " && tmux paste-buffer -p -t " .. pane .. " && tmux send-keys -t " .. pane .. " Enter"
+  local cmd = "tmux load-buffer " .. tmpname .. " && tmux paste-buffer -p -t " .. pane .. " && tmux send-keys -t " .. pane .. " enter"
   vim.fn.system(cmd)
 
-  -- Clean up the temporary file
   os.remove(tmpname)
 end
 
@@ -34,21 +27,41 @@ end
 
 local function send_selection()
   local pane = 0
-  -- i(vim.fn.mode())
   local s = vim.fn.getpos("v")
   local e = vim.fn.getpos(".")
   local lines = vim.fn.getregion(s, e, {type = vim.fn.mode()})
-  -- i(lines)
-  send_to_tmux(lines, pane)
+  send_to_tmux(table.concat(lines, "\n"), pane)
 end
 
 local function send_buffer()
   local pane = 0
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  -- i(lines)
-  send_to_tmux(lines, pane)
+  send_to_tmux(table.concat(lines, "\n"), pane)
 end
 
-vim.keymap.set("v", "<leader>j", function() send_selection() end)
-vim.keymap.set("n", "<leader>l", function() send_line() end)
-vim.keymap.set("n", "<leader>b", function() send_buffer() end)
+vim.keymap.set("v", "<M-j>", function() send_selection() end)
+vim.keymap.set("n", "<M-l>", function() send_line() end)
+vim.keymap.set("n", "<M-b>", function() send_buffer() end)
+
+local function find_function_clause(node)
+  if node == nil then return nil
+  elseif node:type() == "module" then return nil
+  elseif node:type() == "function_definition" then return node
+  else return find_function_clause(node:parent()) end
+end
+
+vim.keymap.set("n", "<M-k>", function ()
+  local ts = require("vim.treesitter")
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local curr_node = ts_utils.get_node_at_cursor()
+  local function_node = find_function_clause(curr_node)
+
+  if function_node == nil then
+    print("No surrounding function")
+    return
+  end
+
+  local pane = 0
+  local text = ts.get_node_text(function_node, 0)
+  send_to_tmux(text, pane)
+end)
