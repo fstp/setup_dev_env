02d68e711a237354dc712e2fc7483a65a88a43bd
fstp <john.jb.blomberg@gmail.com>
2024-11-18 00:12:56 +0100

    ansible: Fix various issues with local install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status.
+set -o pipefail # Exit if any command in a pipeline fails.
+sudo apt update
 sudo apt install python3
 sudo apt install python-is-python3
+sudo apt install python3-pip
 sudo apt install python3-venv
-pip install ansible
+sudo apt install pipx
+pipx install --include-deps ansible
+pipx ensurepath
