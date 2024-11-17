0abdc33bb86736a670b11ae2e5db0fb91fd64cd1
John Blomberg <john.jb.blomberg@gmail.com>
2024-11-17 23:20:59 +0100

    Fix .wezterm.lua
    Fixed bug where config was overwritten by config_builder instead of using the builder as a base.

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,13 @@ local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 config.wsl_domains = {
   {
     -- The name of this specific domain.  Must be unique amonst all types
@@ -15,12 +22,6 @@ config.wsl_domains = {
     distribution = 'Ubuntu',
   },
 }
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
 
 config.default_domain = 'WSL:Ubuntu'
 config.font = wezterm.font 'Terminess Nerd Font'
