fa7392aaf59b9f7e0d51721bd856283d84cdfde4
John Blomberg <john.jb.blomberg@gmail.com>
2025-05-13 13:26:54 +0200

    nvim: Add flit plugin for labeled jump to f/t motions

---
+++ nvim/lua/plugins/flit-nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "ggandor/flit.nvim",
+  lazy = false,
+  config = function ()
+    require('flit').setup {
+      keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+      -- A string like "nv", "nvo", "o", etc.
+      labeled_modes = "v",
+      -- Repeat with the trigger key itself.
+      clever_repeat = true,
+      multiline = true,
+      -- Like `leap`s similar argument (call-specific overrides).
+      -- E.g.: opts = { equivalence_classes = {} }
+      opts = {}
+    }
+  end
+}
